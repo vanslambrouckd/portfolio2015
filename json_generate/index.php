76b5c5a9a14e11fe00e77cea4d11b305c599dfbd
david vanslambrouck <vanslambrouckd@users.noreply.github.com>
2015-03-05 17:08:45 +0100

    update

---
+++ json_generate/index.php
@@ -0,0 +1,218 @@
+<?php
+$arr =  array();
+
+$item = array(
+	'title' => 'Agendasysteem scholen roeselare',
+	'url' => '',
+	'omschrijving' => 'Agendasysteem voor 3 scholen in Roeselare'
+);
+$item['screenshots'] = array('agendasysteem/weekrooster.jpg', 'agendasysteem/dashboard.jpg', 'agendasysteem/dagoverzicht.jpg', 'agendasysteem/doelstellingen.jpg', 'agendasysteem/fiches.jpg', 'agendasysteem/fichesdelen.jpg', 'agendasysteem/fichesdelen2.jpg', 'agendasysteem/lesgang.jpg', 'agendasysteem/roosterthemas.jpg', 'agendasysteem/sjablonen.jpg', 'agendasysteem/statistieken.jpg', 'agendasysteem/tijdstiptoevoegen.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'ROSOG Media module',
+	'url' => '',
+	'omschrijving' => 'Media systeem'
+);
+$item['screenshots'] = array('rosog.jpg', 'rosog2.jpg', 'rosog3.jpg', 'rosog4.jpg', 'rosog5.jpg', 'rosog6.jpg', 'rosog7.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Alert! Vorming & preventie',
+	'url' => 'http://www.alert-ehbo.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('alert.jpg', 'alert-mobile.jpg');
+array_push($arr, $item);
+
+
+$item = array(
+	'title' => 'Aquatherm',
+	'url' => 'http://www.aquatherm-belux.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('aquatherm.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Architectencongress 2014',
+	'url' => '',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('architectencongres.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'De Selfie fiets',
+	'url' => 'http://selfiefiets.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('selfiefiets.jpg', 'selfiefiets2.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Tweedehands kinderboetiek',
+	'url' => 'http://tweedehandskinderboetiek.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('tweedehandskinderboetiek.jpg', 'tweedehandskinderboetiek2.jpg', 'tweedehandskinderboetiek3.jpg', 'tweedehandskinderboetiek4.jpg', 'tweedehandskinderboetiek5.jpg', 'tweedehandskinderboetiek-mobile.jpg', 'tweedehandskinderboetiek-mobile2.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Boas VZW',
+	'url' => 'http://boasvzw.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('boasvzw.jpg', 'boasvzw-mobile.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'IHKA',
+	'url' => 'http://www.ihka.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('ihka.jpg', 'ihka2.jpg', 'ihka3.jpg', 'ihka-mobile.jpg', 'ihka-mobile2.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Boetiek techniek',
+	'url' => '',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('boetiektechniek.jpg', 'boetiektechniek2.jpg', 'boetiektechniek3.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Waarom UNIZO',
+	'url' => 'http://waaromunizo.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('waaromunizo.jpg', 'waaromunizo2.jpg', 'waaromunizo3.jpg', 'waaromunizo-mobile.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Combobase Home',
+	'url' => 'http://combobasehome.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('combobasehome.jpg', 'combobase-mobile.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'KFV Frans Vlaanderen',
+	'url' => 'http://www.kfv-fransvlaanderen.org',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('kfv-fransvl.jpg', 'kfv-fransvl-mobile.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'MINT Europe',
+	'url' => 'http://www.minteurope.com',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('minteurope.jpg', 'minteurope2.jpg', 'minteurope-mobile.jpg');
+array_push($arr, $item);
+
+
+$item = array(
+	'title' => 'Pips pillino',
+	'url' => 'http://www.pips-pillino.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('pips.jpg', 'pips2.jpg', 'pips3.jpg', 'pips-mobile.jpg', 'pips-mobile2.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Wabaloe',
+	'url' => 'http://www.wabaloe.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('wabaloe.jpg', 'wabaloe2.jpg', 'wabaloe3.jpg', 'wabaloe-mobile.jpg', 'wabaloe-mobile2.jpg', 'wabaloe-mobile3.jpg');
+array_push($arr, $item);
+
+$item = array(
+	'title' => 'Scoutsival',
+	'url' => 'http://scoutsival.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('scoutsival.jpg', 'scoutsival2.jpg', 'scoutsival3.jpg', 'scoutsival4.jpg', 'scoutsival-mobile1.jpg', 'scoutsival-mobile2.jpg', 'scoutsival-mobile3.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Starter van het jaar',
+	'url' => 'http://startervanhetjaar.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('startervanhetjaar.jpg', 'startervanhetjaar2.jpg', 'startervanhetjaar-mobile.jpg', 'startervanhetjaar-mobile2.jpg', 'startervanhetjaar-mobile3.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Vishandel Neptunus',
+	'url' => 'http://vishandel-neptunus.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('visnept.jpg', 'visnept2.jpg', 'visnept-mobile.jpg', 'visnept-mobile2.jpg', 'visnept-mobile3.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Energyspot',
+	'url' => 'http://energyspot.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('energyspot.jpg', 'energyspot-mobile.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Midwest2020',
+	'url' => 'http://www.midwest2020.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('midwest2020.jpg', 'midwest2020-2.jpg', 'midwest2020-mobile.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Lartduvin',
+	'url' => 'http://lartduvin.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('lartduvin.jpg', 'lartduvin2.jpg', 'lartduvin3.jpg', 'lartduvin4.jpg', 'lartduvin5.jpg', 'lartduvin6.jpg', 'lartduvin-mobile.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Lovan RC',
+	'url' => 'http://lovanrc-team.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('lovanrc.jpg', 'lovanrc2.jpg', 'lovanrc3.jpg', 'lovanrc4.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Neem de handschoen op',
+	'url' => 'http://neemdehandschoenop.be',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('neemdehandschoenop.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Bedatbeselare',
+	'url' => 'http://bedatbeselare.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('bedatbeselare.jpg');
+array_push($arr, $item);
+
+ $item = array(
+	'title' => 'Garnifood',
+	'url' => 'http://garnifood.be/',
+	'omschrijving' => ''
+);
+$item['screenshots'] = array('garnifood.jpg');
+array_push($arr, $item);
+
+foreach($arr as $index => $item) {
+	$arr[$index]['id'] = $index;
+}
+echo stripslashes(json_encode($arr));
+?>
